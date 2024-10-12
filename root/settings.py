cb81e2a0a0d7f8d36d208c847c4f9609b4b6d4df
Joe Philip <joe.philip@hotmail.co.in>
2024-10-12 10:00:20 +0000

    modified settings.py
    modified  variable to use from  file
    
    modified  variable to use from  file

--- root/settings.py
+++ root/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-8nu0(*7_!vks+x5llae%f&c78bi5=exs-o(ln%on&c*$tng%@g"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = []
 
