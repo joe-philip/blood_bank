2ed84fa5f678209aab76ea6368130e4612d3ec88
Joe Philip <joe.philip@hotmail.co.in>
2024-10-14 11:44:22 +0000

    removed lazy function call

--- main/models.py
+++ main/models.py
@@ -25,9 +25,9 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         if extra_fields.get('is_staff') is not True:
-            raise ValueError(_('Superuser must have is_staff=True.'))
+            raise ValueError('Superuser must have is_staff=True.')
         if extra_fields.get('is_superuser') is not True:
-            raise ValueError(_('Superuser must have is_superuser=True.'))
+            raise ValueError('Superuser must have is_superuser=True.')
         return self.create_user(**extra_fields)
 
 
