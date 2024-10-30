9d2f58f904c0d9966c385ffdfbc8ebe87677baed
Joe Philip <joe.philip@hotmail.co.in>
2024-10-30 14:38:06 +0000

    defined URL for BloodGroupsListAPIView

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup', views.SignupAPIView.as_view())
+    path('signup', views.SignupAPIView.as_view()),
+    path('blood_groups', views.BloodGroupsListAPIView.as_view())
 ]
