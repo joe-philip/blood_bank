d9a841b065df38886b5ddf4255eab40677a6c3eb
Joe Philip <joe.philip@hotmail.co.in>
2024-10-30 14:45:24 +0000

    defined URL for RolesListAPIView

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('signup', views.SignupAPIView.as_view()),
-    path('blood_groups', views.BloodGroupsListAPIView.as_view())
+    path('blood_groups', views.BloodGroupsListAPIView.as_view()),
+    path('roles', views.RolesListAPIView.as_view())
 ]
