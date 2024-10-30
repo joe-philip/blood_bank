e067bfb5a29a39a2f5a3163f3a60a9134b0adc54
Joe Philip <joe.philip@hotmail.co.in>
2024-10-30 15:26:28 +0000

    defined LoginAPIView

--- main/views.py
+++ main/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import authenticate
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.generics import ListAPIView
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -5,8 +7,11 @@ from rest_framework.views import APIView
 
 from admin_user.models import BloodGroups, Roles
 
-from .serializers import (BloodGroupsSerializer, RolesSerializer,
-                          SignupSerializer, UserObjectSerializer)
+from .models import User
+from .serializers import (BloodGroupsSerializer, LoginSerializer,
+                          RolesSerializer, SignupSerializer,
+                          UserObjectSerializer)
+from .utils import get_token_for_user
 
 # Create your views here.
 
@@ -27,3 +32,22 @@ class BloodGroupsListAPIView(ListAPIView):
 class RolesListAPIView(ListAPIView):
     serializer_class = RolesSerializer
     queryset = Roles.objects.all()
+
+
+class LoginAPIView(APIView):
+    def post(self, request: Request) -> Response:
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = authenticate(
+            request,
+            email=serializer.validated_data.get('email'),
+            password=serializer.validated_data.get('password')
+        )
+        if user:
+            token = get_token_for_user(user)
+            data = UserObjectSerializer(user).data
+            data['token'] = {
+                'access': token.key
+            }
+            return Response(data)
+        raise AuthenticationFailed('Bad Credentials')
