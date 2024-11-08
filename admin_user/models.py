e38f37ccbd2ae36549f217dc35108bbd9a4b6265
Joe Philip <joe.philip@hotmail.co.in>
2024-11-08 13:31:21 +0000

    Defined Countries model

--- admin_user/models.py
+++ admin_user/models.py
@@ -25,3 +25,27 @@ class Roles(MetaModel):
         verbose_name = 'role'
 
     def __str__(self) -> str: return self.name
+
+
+class Countries(MetaModel):
+    common_name = models.CharField(max_length=50)
+    official_name = models.CharField(max_length=250)
+    iso2 = models.CharField(max_length=2)
+    iso3 = models.CharField(max_length=3)
+    CIOC_DESCRIPTION = 'Code of the International Olympic Committee'
+    cioc = models.CharField(
+        db_comment=CIOC_DESCRIPTION,
+        help_text=CIOC_DESCRIPTION
+    )
+    flag = models.CharField(max_length=5)
+    timezones = models.JSONField(default=dict)
+    pincode_regex = models.CharField(max_length=10)
+    phone_codes = models.JSONField(default=dict)
+
+    class Meta:
+        db_table = 'countries'
+        verbose_name = 'Country'
+        verbose_name_plural = 'Countries'
+
+    def __str__(self) -> str:
+        return self.common_name
